8929f796a51f598bbddd4f9d0a89615007bef709
Balazs Varga <varbalazs@gmail.com>
2017-02-04 19:52:31 +0100

    Add additional accounts to banks, adjust balances to account for cents
    > Create an initial system consisting of two banks with at least two accounts each.

--- db/seed.rb
+++ db/seed.rb
@@ -11,7 +11,10 @@ class SeedData
     beta_id = banks.insert(name: "Beta")
 
     accounts = database.from(:accounts)
-    accounts.insert(name: "Jim's account", bank_id: alpha_id, balance_in_cents: 30_000)
-    accounts.insert(name: "Emma's account", bank_id: beta_id, balance_in_cents: 2_500)
+    accounts.insert(name: "Jim's account", bank_id: alpha_id, balance_in_cents: 300_000)
+    accounts.insert(name: "Other alpha account", bank_id: alpha_id, balance_in_cents: 1_000)
+
+    accounts.insert(name: "Emma's account", bank_id: beta_id, balance_in_cents: 25_000)
+    accounts.insert(name: "Other beta account", bank_id: beta_id, balance_in_cents: 500)
   end
 end
